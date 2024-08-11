cd37a8376f426c15de8c96a013f6c29b837a5a40
Rin-Bruh <quytt.21it@vku.udn.vn>
2024-08-11 11:02:58 +0700

    CRUD-login-register

---
+++ Booky.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using Booky.DataAccess.Data;
+using Booky.DataAccess.Repository.IRepository;
+using Booky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booky.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Update(Product obj)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Price = obj.Price;
+                objFromDb.Price200 = obj.Price200;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price500 = obj.Price500;
+                objFromDb.Description = obj.Description;
+                objFromDb.CategoryId = obj.CategoryId;
+                objFromDb.Author = obj.Author;
+                //objFromDb.ProductImages = obj.ProductImages;
+                if (obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
+        }
+    }
+}
---
+++ Booky.Models/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace Booky.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [Display(Name = "List Price")]
+        [Range(1000, 1000000)]
+        public double ListPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 1000-200000")]
+        [Range(1000, 1000000)]
+        public double Price { get; set; }
+
+
+        [Required]
+        [Display(Name = "Price for 200000+")]
+        [Range(1000, 1000000)]
+        public double Price200 { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 500000+")]
+        [Range(1000, 1000000)]
+        public double Price500 { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        [ValidateNever]
+        public Category Category { get; set; }
+        [ValidateNever]
+        public string ImageUrl { get; set; }
+    }
+}
