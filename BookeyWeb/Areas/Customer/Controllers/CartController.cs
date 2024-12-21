20debb12357f82fc238a7eb359ad9288787ad2f1
Rin-Bruh <quytt.21it@vku.udn.vn>
2024-12-21 18:41:07 +0700

    ConfirmStripePayment

--- BookeyWeb/Areas/Customer/Controllers/CartController.cs
+++ BookeyWeb/Areas/Customer/Controllers/CartController.cs
@@ -3,7 +3,9 @@ using Booky.Models;
 using Booky.Models.ViewModels;
 using Booky.Utility;
 using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
 using System.Security.Claims;
+using static System.Net.WebRequestMethods;
 
 namespace BookeyWeb.Areas.Customer.Controllers
 {
@@ -121,8 +123,41 @@ namespace BookeyWeb.Areas.Customer.Controllers
 
 			if (applicationUser.CompanyId.GetValueOrDefault() == 0)
 			{
-				//it is a regular customer 
+				//it is a regular customer account and we need to capture payment
+				//stripe logic
+				//var domain = "https://localhost:7046/";
+				//var options = new SessionCreateOptions
+				//{
+				//	SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
+				//	CancelUrl = domain + "customer/cart/index",
+				//	LineItems = new List<SessionLineItemOptions>(),
+				//	Mode = "payment",
+				//};
 
+				//foreach (var item in ShoppingCartVM.ShoppingCartList)
+				//{
+				//	var sessionLineItem = new SessionLineItemOptions
+				//	{
+				//		PriceData = new SessionLineItemPriceDataOptions
+				//		{
+				//			UnitAmount = (long)(item.Price * 1000), // 200.000 => 200000
+				//			Currency = "usd",
+				//			ProductData = new SessionLineItemPriceDataProductDataOptions
+				//			{
+				//				Name = item.Product.Title
+				//			}
+				//		},
+				//		Quantity = item.Count
+				//	};
+				//	options.LineItems.Add(sessionLineItem);
+				//}
+				//var service = new SessionService();
+				//Session session = service.Create(options);
+
+				//_unitOfWork.OrderHeader.UpdateStripePaymentID(ShoppingCartVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+				//_unitOfWork.Save();
+				//Response.Headers.Add("Location", session.Url);
+				//return new StatusCodeResult(303);
 			}
 			return RedirectToAction(nameof(OrderConfirmation), new { id = ShoppingCartVM.OrderHeader.Id });
 		
@@ -130,7 +165,33 @@ namespace BookeyWeb.Areas.Customer.Controllers
 
         public IActionResult OrderConfirmation(int id)
         {
-            return View(id);
+			//OrderHeader orderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == id, includeProperties: "ApplicationUser");
+			//if (orderHeader.PaymentStatus != SD.PaymentStatusDelayedPayment)
+			//{
+			//	//this is an order by customer
+
+			//	var service = new SessionService();
+			//	Session session = service.Get(orderHeader.SessionId);
+
+			//	if (session.PaymentStatus.ToLower() == "paid")
+			//	{
+			//		_unitOfWork.OrderHeader.UpdateStripePaymentID(id, session.Id, session.PaymentIntentId);
+			//		_unitOfWork.OrderHeader.UpdateStatus(id, SD.StatusApproved, SD.PaymentStatusApproved);
+			//		_unitOfWork.Save();
+			//	}
+			//	HttpContext.Session.Clear();
+
+			//}
+
+			//_emailSender.SendEmailAsync(orderHeader.ApplicationUser.Email, "New Order - Bulky Book",
+			//	$"<p>New Order Created - {orderHeader.Id}</p>");
+
+			//List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart
+			//	.GetAll(u => u.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
+
+			//_unitOfWork.ShoppingCart.RemoveRange(shoppingCarts);
+			//_unitOfWork.Save();
+			return View(id);
         }
 		public IActionResult Plus(int cartId)
         {
