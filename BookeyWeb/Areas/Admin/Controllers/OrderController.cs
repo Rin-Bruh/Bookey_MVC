f472931c3107c06e46a9cc8500b9e3d4bbd3225d
Rin-Bruh <quytt.21it@vku.udn.vn>
2024-12-24 19:39:40 +0700

    OrderDetailGetAction

--- BookeyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BookeyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,5 +1,7 @@
 ﻿using Booky.DataAccess.Repository.IRepository;
 using Booky.Models;
+using Booky.Models.ViewModels;
+using Booky.Utility;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +11,8 @@ namespace BookeyWeb.Areas.Admin.Controllers
 	public class OrderController : Controller
 	{
 		private readonly IUnitOfWork _unitOfWork;
+		[BindProperty]
+		public OrderVM OrderVM { get; set; }
 		public OrderController(IUnitOfWork unitOfWork)
 		{
 			_unitOfWork = unitOfWork;
@@ -17,14 +21,44 @@ namespace BookeyWeb.Areas.Admin.Controllers
 		{
 			return View();
 		}
+		public IActionResult Details(int orderId)
+		{
+			OrderVM = new()
+			{
+				OrderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+				OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
+			};
+
+			return View(OrderVM);
+		}
 
 		#region API CALLS
 
 		[HttpGet]
-		public IActionResult GetAll()
+		public IActionResult GetAll(string status)
 		{
-			List<OrderHeader> objOrderHeader = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
-			return Json(new { data = objOrderHeader });
+			IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+			switch (status)
+			{
+				case "pending":
+					objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+					break;
+				case "inprocess":
+					objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+					break;
+				case "completed":
+					objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+					break;
+				case "approved":
+					objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+					break;
+				default:
+					break;
+
+			}
+
+			return Json(new { data = objOrderHeaders });
 		}
 
 		#endregion
