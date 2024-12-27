a927887c8e8aa8cb2cbbbc2470bccd2d87595b49
Rin-Bruh <quytt.21it@vku.udn.vn>
2024-12-27 20:03:06 +0700

    OrderProcessingButtonsLogic

--- BookeyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BookeyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -5,6 +5,7 @@ using Booky.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BookeyWeb.Areas.Admin.Controllers
 {
@@ -64,7 +65,22 @@ namespace BookeyWeb.Areas.Admin.Controllers
 		[HttpGet]
 		public IActionResult GetAll(string status)
 		{
-			IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			IEnumerable<OrderHeader> objOrderHeaders;
+
+			//check the role of the logged in user
+			if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+			{
+				objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			}
+			else
+			{
+
+				var claimsIdentity = (ClaimsIdentity)User.Identity;
+				var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+				objOrderHeaders = _unitOfWork.OrderHeader
+					.GetAll(u => u.ApplicationUserId == userId, includeProperties: "ApplicationUser");
+			}
 
 			switch (status)
 			{
