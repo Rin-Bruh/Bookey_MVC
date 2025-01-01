245c40700bd3a939273d304f127a15d7d70befd5
Rin-Bruh <quytt.21it@vku.udn.vn>
2025-01-01 18:42:37 +0700

    RemoveSessionAndBugSolution

--- BookeyWeb/Areas/Customer/Controllers/CartController.cs
+++ BookeyWeb/Areas/Customer/Controllers/CartController.cs
@@ -204,14 +204,14 @@ namespace BookeyWeb.Areas.Customer.Controllers
 
         public IActionResult Minus(int cartId)
         {
-            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId, tracked: true);
             if (cartFromDb.Count <= 1)
             {
                 //remove that from cart
 
                 _unitOfWork.ShoppingCart.Remove(cartFromDb);
-                //HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
-                //    .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
+                HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
+                    .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
             }
             else
             {
@@ -225,12 +225,12 @@ namespace BookeyWeb.Areas.Customer.Controllers
 
         public IActionResult Remove(int cartId)
         {
-            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId, tracked:true);
 
             _unitOfWork.ShoppingCart.Remove(cartFromDb);
 
-            //HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
-            //  .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
+            HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
+              .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
             _unitOfWork.Save();
             return RedirectToAction(nameof(Index));
         }
