be19297eb762f5ecfaeb446fbecacdeae3a82a7c
Rin-Bruh <quytt.21it@vku.udn.vn>
2025-01-11 11:02:50 +0700

    AddProductImageRepository

---
+++ Booky.DataAccess/Repository/ProductImageRepository.cs
@@ -0,0 +1,26 @@
+﻿using Booky.DataAccess.Data;
+using Booky.DataAccess.Repository.IRepository;
+using Booky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booky.DataAccess.Repository
+{
+    public class ProductImageRepository : Repository<ProductImage>, IProductImageRepository
+	{
+        private ApplicationDbContext _db;
+        public ProductImageRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Update(ProductImage obj)
+        {
+            _db.ProductImages.Update(obj);
+        }
+    }
+}
